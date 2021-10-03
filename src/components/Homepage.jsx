304c068b41e22095bda9250659f56d0e1c684e9d
Kailash R <kseervi257@gmail.com>
2021-10-03 12:39:44 +0530

    Cryptocurrencies

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -4,11 +4,14 @@ import { Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
 
 import { useGetCryptosQuery } from '../services/cryptoApi';
+import { Cryptocurrencies, News } from '../components/index';
+
 
 const { Title } = Typography;
 
 const Homepage = () => {
-    const { data, isFetching } = useGetCryptosQuery();
+    const { data, isFetching } = useGetCryptosQuery(10);
+    console.log(data);
     const globalStats = data?.data?.stats;
 
     if (isFetching) return 'Loading...';
@@ -49,6 +52,25 @@ const Homepage = () => {
             />
           </Col>
         </Row>
+        <div className="home-heading-container">
+          <Title level={2} className="home-title">
+            Top 10 Cryptocurrencies in the world
+          </Title>
+          <Title level={3} className="show-more">
+            <Link to="/cryptocurrencies">Show More</Link>
+          </Title>
+        </div>
+        <Cryptocurrencies simplified/>
+
+        <div className="home-heading-container">
+          <Title level={2} className="home-title">
+            Latest Crypto News
+          </Title>
+          <Title level={3} className="show-more">
+            <Link to="/news">Show More</Link>
+          </Title>
+        </div>
+        <News simplified />
       </React.Fragment>
     );
 }
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -10,14 +10,14 @@ const baseUrl = "https://coinranking1.p.rapidapi.com";
 const createRequest = (url) => ({url, headers: cryptoApiHeaders})
 
 export const cryptoApi = createApi({
-    reducerPath: 'cryptoApi',
-    baseQuery: fetchBaseQuery({ baseUrl }),
-    endpoints: (builder) => ({
-        getCryptos: builder.query({
-            query:  () => createRequest('/coins')
-        })
-    })
-})
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`),
+    }),
+  }),
+});
 
 export const {
     useGetCryptosQuery,
